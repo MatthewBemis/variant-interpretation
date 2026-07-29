dab7657c4a3eab0b34986cc19991d02f51315297
Matt Bemis <mbemis@broadinstitute.org>
2026-07-29 01:22:13 -0400

    add scrolltotop

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,13 +1,17 @@
 import { Route, Routes } from "react-router-dom";
+import ScrollToTop from "./components/ScrollToTop";
 import SearchEntryPage from "./pages/SearchEntryPage";
 import SearchResultsPage from "./pages/SearchResultsPage";
 
 function App() {
   return (
-    <Routes>
-      <Route path="/" element={<SearchEntryPage />} />
-      <Route path="/results" element={<SearchResultsPage />} />
-    </Routes>
+    <>
+      <ScrollToTop />
+      <Routes>
+        <Route path="/" element={<SearchEntryPage />} />
+        <Route path="/results" element={<SearchResultsPage />} />
+      </Routes>
+    </>
   );
 }
 
